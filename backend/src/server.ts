1ae85f16dd9a22ea4b8015800ec6426d85785c26
Ayo Omotosho <aomotosho4@gmail.com>
2023-07-16 14:28:45 -0500

    test commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,11 @@ import { config } from "dotenv";
 
 config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://bug-tracker-api.vercel.app/"],
+  })
+);
 app.use(express.json());
 mongoose.set("strictQuery", true);
 
