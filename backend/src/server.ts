b20c994ea57d1cb9bb1b17366a795da617c3f872
Ayo Omotosho <aomotosho4@gmail.com>
2023-07-16 14:10:42 -0500

    configure vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,20 @@
-import express, { Request, Response} from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import {config} from 'dotenv'
-import User from './models/userModel'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { config } from "dotenv";
 
+config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose.set("strictQuery", true);
 
-config()
-const app = express()
-app.use(cors())
-app.use(express.json())
-mongoose.set('strictQuery', true);
+app.use("/", require("./routes/projectRoute"));
+app.use("/", require("./routes/ticketRoute"));
+app.use("/", require("./routes/userRoute"));
+app.use("/", require("./routes/teamRoute"));
 
-
-
-app.use("/", require("./routes/projectRoute"))
-app.use("/", require("./routes/ticketRoute"))
-app.use("/", require("./routes/userRoute"))
-app.use("/", require("./routes/teamRoute"))
-
-
-mongoose
-  .connect(process.env.MONGO_URL!)
-  .then(() => {
-    console.log(`listening on port: ${process.env.PORT}`);
-    app.listen(process.env.PORT)
-  })
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port: ${process.env.PORT}`);
+  app.listen(process.env.PORT);
+});
